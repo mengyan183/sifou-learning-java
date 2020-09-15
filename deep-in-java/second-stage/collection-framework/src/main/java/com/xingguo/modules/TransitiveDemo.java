fed18363441c690d6a5b366d0ad16bc55197a31d
xingguo <xingguo0828@gmail.com>
2020-09-15 10:28:23 +0800

    模块化依赖传递学习以及模块路径的学习

--- deep-in-java/second-stage/collection-framework/src/main/java/com/xingguo/modules/TransitiveDemo.java
+++ deep-in-java/second-stage/collection-framework/src/main/java/com/xingguo/modules/TransitiveDemo.java
@@ -14,7 +14,21 @@ import java.util.logging.Logger;
  * @since
  */
 public class TransitiveDemo {
+    /**
+     * 可以看到在当前的启动命令中 存在 模块路径 以及 具体的jar包
+     *
+     * -p /Users/xingguo/sifou-learning-java/deep-in-java/second-stage/collection-framework/target/classes:
+     * /Users/xingguo/.m2/repository/org/springframework/spring-context/5.2.8.RELEASE/spring-context-5.2.8.RELEASE.jar:
+     * /Users/xingguo/.m2/repository/org/springframework/spring-aop/5.2.8.RELEASE/spring-aop-5.2.8.RELEASE.jar:
+     * /Users/xingguo/.m2/repository/org/springframework/spring-beans/5.2.8.RELEASE/spring-beans-5.2.8.RELEASE.jar:
+     * /Users/xingguo/.m2/repository/org/springframework/spring-core/5.2.8.RELEASE/spring-core-5.2.8.RELEASE.jar:
+     * /Users/xingguo/.m2/repository/org/springframework/spring-jcl/5.2.8.RELEASE/spring-jcl-5.2.8.RELEASE.jar:
+     * /Users/xingguo/.m2/repository/org/springframework/spring-expression/5.2.8.RELEASE/spring-expression-5.2.8.RELEASE.jar
+     * @param args
+     */
     public static void main(String[] args) {
-        Logger logger ;// 由于依赖了java.sql中存在 transitive java.logging,因此可以直接使用java.logging中的代码, 而当不直接或间接依赖 java.logging时,当前代码会编译错误
+        Logger logger;// 由于依赖了java.sql中存在 transitive java.logging,因此可以直接使用java.logging中的代码, 而当不直接或间接依赖 java.logging时,当前代码会编译错误
+        logger = Logger.getLogger("TransitiveDemo");
+        logger.info("1");
     }
 }
--- deep-in-java/second-stage/collection-framework/src/main/java/module-info.java
+++ deep-in-java/second-stage/collection-framework/src/main/java/module-info.java
@@ -1,8 +1,10 @@
 /*
  * Copyright (c) 2020, guoxing, Co,. Ltd. All Rights Reserved
  */
+//module java.base{// 当存在模板路径名和依赖路径名重名时会出现依赖冲突,提示循环依赖
 module collection.framework { //定义当前模块的名称类似于maven的artifactId
     requires java.base; // 默认依赖, requires 后面加的是模块的模块名而非包名
     requires java.sql; // 在java.sql中的requires transitive 表示了依赖传递, 当引入 java.sql依赖后,会默认将java.sql中使用 requires transitive 的相关依赖也传递进来
     exports com.xingguo.modules; // 要暴露的当前模块的包下的类,这里要求导出包下必须存在java文件
+    uses java.sql.Driver; // 表示使用引入包中的具体的类
 }
